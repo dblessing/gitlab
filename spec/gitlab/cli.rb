4168b6966b2494e4764f7564769146aaa5f44bbe
Drew Blessing <drew.blessing@me.com>
2014-05-05 06:34:56 -0500

    Add tests for alias

--- spec/gitlab/cli.rb
+++ spec/gitlab/cli.rb
@@ -2,8 +2,24 @@ require 'spec_helper'
 
 describe Gitlab::CLI do
   describe "version" do
-    subject { cli_action('-v') }
+    before { @version_string = "Gitlab gem version #{Gitlab::VERSION}\n" }
 
-    it { should == "Gitlab gem version #{Gitlab::VERSION}\n" }
+    context "with action name" do
+      subject { cli_action('version') }
+      
+      it { should == @version_string }
+    end
+
+    context "with short arg" do
+      subject { cli_action('-v') }
+
+      it { should == @version_string }
+    end
+
+    context "with long arg" do
+      subject { cli_action('--version') }
+
+      it { should == @version_string }
+    end
   end
 end
