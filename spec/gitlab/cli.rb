1e2a19922f2e372840839b7e1e5e1cc48a8f0887
Drew Blessing <drew.blessing@me.com>
2014-05-06 19:58:33 -0500

    Refactor tests

--- spec/gitlab/cli.rb
+++ spec/gitlab/cli.rb
@@ -4,33 +4,22 @@ describe Gitlab::CLI do
   describe "version" do
     before { @version_string = "Gitlab gem version #{Gitlab::VERSION}\n" }
 
-    context "with action name" do
-      subject { cli_action('version') }
-      
-      it { should == @version_string }
-    end
-
-    context "with short arg" do
-      subject { cli_action('-v') }
-
-      it { should == @version_string }
-    end
-
-    context "with long arg" do
-      subject { cli_action('--version') }
-
-      it { should == @version_string }
+    it "returns the version" do
+      expect(cli_action('version')).to be == @version_string
+      expect(cli_action('-v')).to be == @version_string
+      expect(cli_action('--version')).to be == @version_string
     end
   end
 
   describe "projects" do
-    subject { cli_action('projects') }
-
     before do
       stub_get("/projects", "projects")
       @projects = Gitlab.projects
     end
 
-    it { should == Gitlab::CLI.new.formattsed_projects(@projects) }
+    it "returns formatted projects" do
+      expect(cli_action('projects'))
+          .to be == Gitlab::CLI.new.formatted_projects(@projects)
+    end
   end
 end
